3f4b2262664b81511c0bf34dc75c13cfd28f8ad0
Max Dawkins <max.dawkins@gmail.com>
2025-01-30 11:50:24 -0600

    [Tuner] Add support for more convolution layouts
    Signed-off-by: Max Dawkins <max.dawkins@gmail.com>

--- tuner/tuner/candidate_gen.py
+++ tuner/tuner/candidate_gen.py
@@ -42,6 +42,7 @@ class DispatchTuner(DispatchParser):
         ir_module: ir.Module,
         compilation_info: iree_codegen.CompilationInfoAttr,
         args: argparse.Namespace,
+        problem_size: Optional[ProblemSize],
     ) -> ir.Module:
         """Generate a transform dialect spec that applies the compilation info attr."""
         pass
@@ -68,6 +69,7 @@ class ContractionOpInterfaceTuner(DispatchTuner, ContractionOpInterfaceParser):
         ir_module: ir.Module,
         compilation_info: iree_codegen.CompilationInfoAttr,
         args: argparse.Namespace,
+        problem_size: Optional[ProblemSize],
     ) -> ir.Module:
         contraction_op: ir.Operation = self.get_contraction_operation(ir_module)
         lhs_type = ir.ShapedType(contraction_op.operands[0].type)
@@ -89,24 +91,25 @@ class ConvolutionOpInterfaceTuner(DispatchTuner, ConvolutionOpInterfaceParser):
         ir_module: ir.Module,
         compilation_info: iree_codegen.CompilationInfoAttr,
         args: argparse.Namespace,
+        problem_size: Optional[ProblemSize],
     ) -> ir.Module:
+        assert problem_size, "Problem size not found"
+
         conv_op: ir.Operation = self.get_conv_operation(ir_module)
-        assert (
-            conv_op.name == "linalg.conv_2d_nhwc_hwcf"
-        ), "expected linalg.conv_2d_nhwc_hwcf"
         lhs_type = ir.ShapedType(conv_op.operands[0].type)
         rhs_type = ir.ShapedType(conv_op.operands[1].type)
         acc_type = ir.ShapedType(conv_op.operands[2].type)
-        N = acc_type.get_dim_size(0)
-        H = acc_type.get_dim_size(1)
-        W = acc_type.get_dim_size(2)
-        C = rhs_type.get_dim_size(2)
-        P = rhs_type.get_dim_size(0)
-        Q = rhs_type.get_dim_size(1)
-        F = rhs_type.get_dim_size(3)
+
+        M_str = "x".join(map(str, problem_size.matmul_size.M))
+        N_str = "x".join(map(str, problem_size.matmul_size.N))
+        K_str = "x".join(map(str, problem_size.matmul_size.K))
+
         conv_type = conv_op.name.split(".")[-1]
         # TODO(Max191): Get the function name from the func.func in the input module.
-        func_name = f"match_{conv_type}_{N}x{H}x{W}x{C}x{P}x{Q}x{F}_{lhs_type.element_type}x{rhs_type.element_type}x{acc_type.element_type}"
+        func_name = f"match_{conv_type}_{M_str}_{N_str}_{K_str}_"
+        f"{lhs_type.element_type}x{rhs_type.element_type}x{acc_type.element_type}"
+        tune_logger.debug(f"func_name: {func_name}")
+
         return build_td_spec(ir_module.context, conv_op, compilation_info, func_name, args)
 
 
@@ -199,7 +202,7 @@ def generate_configs_and_td_specs(
         if i >= limit:
             break
         tune_logger.debug(f"Solution #{i+1}: {config}")
-        td_spec_module = dispatch_tuner.get_td_spec(input_module, config, args)
+        td_spec_module = dispatch_tuner.get_td_spec(input_module, config, args, problem_size)
         assert td_spec_module, "Failed to generate transform dialect spec"
         config_specs.append(td_spec_module)
 
